2f7d15779d8d1ce5c01e345a256f52a5addbe702
SandeepKumar-05 <sandeepkumarps005@gmail.com>
2026-07-07 20:56:38 +0530

    Updated UI

--- basics/lib/Features/Auth/presentation/pages/signup_page.dart
+++ basics/lib/Features/Auth/presentation/pages/signup_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:basics/core/theme/app_pallete.dart';
+import 'package:basics/Features/Auth/presentation/widgets/auth_field.dart';
+import 'package:basics/Features/Auth/presentation/widgets/auth_gradient_button.dart';
 
 class SignUpPage extends StatefulWidget {
   const SignUpPage({super.key});
@@ -8,13 +11,85 @@ class SignUpPage extends StatefulWidget {
 }
 
 class _SignUpPageState extends State<SignUpPage> {
+  final nameController = TextEditingController();
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  final formKey = GlobalKey<FormState>();
+
+  @override
+  void dispose() {
+    nameController.dispose();
+    emailController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text(
-          ' Page',
-          style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold),
+    return Scaffold(
+      appBar: AppBar(),
+      body: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Form(
+          key: formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                'Sign Up.',
+                style: TextStyle(
+                  fontSize: 50,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 30),
+              AuthField(
+                hintText: 'Name',
+                controller: nameController,
+              ),
+              const SizedBox(height: 15),
+              AuthField(
+                hintText: 'Email',
+                controller: emailController,
+              ),
+              const SizedBox(height: 15),
+              AuthField(
+                hintText: 'Password',
+                controller: passwordController,
+                isObscureText: true,
+              ),
+              const SizedBox(height: 20),
+              AuthGradientButton(
+                buttonText: 'Sign Up',
+                onPressed: () {
+                  if (formKey.currentState!.validate()) {
+                    // Sign up logic
+                  }
+                },
+              ),
+              const SizedBox(height: 20),
+              GestureDetector(
+                onTap: () {
+                  // Navigate to SignInPage
+                },
+                child: RichText(
+                  text: TextSpan(
+                    text: 'Already have an account? ',
+                    style: Theme.of(context).textTheme.titleMedium,
+                    children: [
+                      TextSpan(
+                        text: 'Sign In',
+                        style: Theme.of(context).textTheme.titleMedium?.copyWith(
+                              color: AppPallete.gradient2,
+                              fontWeight: FontWeight.bold,
+                            ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
